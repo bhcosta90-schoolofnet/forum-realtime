e0fb2b204233383c835d3a69980ea8310d3c4c2d
Bruno Henrique da Costa <bhcosta90@gmail.com>
2020-11-07 00:59:21 -0300

    supervisor

--- heroku/script.sh
+++ heroku/script.sh
@@ -4,6 +4,6 @@ if [ "$APP_ENV" != "production" ]
 then
     composer install;
     php artisan test;
+    php artisan migrate:fresh --seed
     composer install --no-dev;
 fi
-php artisan migrate:fresh --seed
